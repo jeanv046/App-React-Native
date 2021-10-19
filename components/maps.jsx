cbca7f7b862a7e5feee2c35769503604e181f56e
Jean Vega <jeanv046@gmail.com>
2021-10-19 18:33:18 -0500

    Mapa y chat en proceso

--- components/maps.jsx
+++ components/maps.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View, navigation, } from "react-native";
+import { ScrollView, StyleSheet, Text, View, navigation, TextInput, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import * as Location from 'expo-location';
 
@@ -43,12 +43,36 @@ const maps = ({ navigation }) => {
     }
     return (
         <View style={styles.container}>
-
             {location && <MapComponent />}
             <View style={styles.cards}>
-
+                <ScrollView contentContainerStyle={styles.contentCard2}>
+                    <View style={styles.contenReceptor}>
+                        <View style={styles.ChekReceptor}>
+                            <Text style={styles.textoChat}>Registrate</Text>
+                        </View>
+                    </View>
+                    <View style={styles.contenEmisor}>
+                        <View style={styles.CheckEmisor}>
+                            <Text style={styles.textoChat}>Registrate</Text>
+                        </View>
+                    </View>
+                    <View style={styles.contenReceptor}>
+                        <View style={styles.ChekReceptor}>
+                            <Text style={styles.textoChat}>Hola como estas? espero que te encuentres bien y que todo salga correcto? como sigue mi tia? esta bien.</Text>
+                        </View>
+                    </View>
+                </ScrollView>
+                <View style={styles.contentSubmit}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Escribe tu Mensaje"
+                    >
+                    </TextInput>
+                    {/* <Image
+                        style={styles.imageEdit}
+                        source={require('../assets/enviar(1).png')}></Image> */}
+                </View>
             </View>
-
         </View>
     );
 };
@@ -59,7 +83,7 @@ const styles = StyleSheet.create({
         width: "100%",
         justifyContent: 'flex-end',
         alignItems: 'center',
-        
+
     },
     map: {
         ...StyleSheet.absoluteFillObject,
@@ -68,17 +92,79 @@ const styles = StyleSheet.create({
         fontSize: 18,
         textAlign: 'center',
     },
-    cards:{
+    cards: {
         width: '95%',
-        height: 250,
-        padding:10,
-        marginLeft:20,
-        marginRight:20,
-        marginBottom: 10,
+        height: 300,
+        padding: 10,
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 20,
         borderRadius: 16,
         backgroundColor: '#002138',
         position: 'absolute',
         bottom: 0,
+    },
+    contentCard2: {
+        width: '100%',
+        display: 'flex',
+        alignItems: 'flex-end',
+        /* backgroundColor: 'black' */
+    },
+    contenReceptor: {
+        width: '100%',
+        padding: 10,
+        display: 'flex',
+        alignItems: 'flex-start',
+    },
+    contenEmisor: {
+        width: '100%',
+        padding: 10,
+        display: 'flex',
+        alignItems: 'flex-end',
+    },
+    ChekReceptor: {
+        maxWidth: '80%',
+        backgroundColor: 'white',
+        borderRadius: 16,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    CheckEmisor: {
+        maxWidth: '80%',
+        backgroundColor: '#AAFEA5',
+        borderRadius: 16,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    textoChat: {
+        fontSize: 18,
+        color: 'black',
+
+    },
+    contentSubmit: {
+        width: '100%',
+        position: 'relative',
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 0,
+        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        borderRadius: 50,
+        backgroundColor: '#FFFFFF',
+
+    },
+    imageEdit: {
+        width: 20,
+        height: 20,
     }
+
+
 });
 export default maps
