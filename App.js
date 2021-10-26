ce2904215a11b121c77de167bcf1b56637be6bac
mauricio martinez <maumarcastil@gmail.com>
2021-10-26 16:49:19 -0500

    implementacion flujo navegacion

--- App.js
+++ App.js
@@ -1,43 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
-import Register from './components/register';
-import Inicio from './components/inicio';
-import maps from './components/maps';
-import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
-import { createNativeStackNavigator, } from '@react-navigation/native-stack';
-import { isLoggedIn } from "./funtion"
-
-
-const Stack = createNativeStackNavigator();
+import React from "react";
+import { AuthProvider } from "./navigation/AuthProvider";
+import Routes from "./navigation/routes";
 
 export default function App() {
-
-  useEffect(() => {
-    isLoggedIn()
-  }, [])
-
-  const MyTheme = {
-    ...DefaultTheme,
-    colors: {
-      ...DefaultTheme.colors,
-      primary: 'rgb(255, 45, 85)',
-      background: '#002138',
-    },
-  };
-
-
-
   return (
-    <NavigationContainer theme={MyTheme}>
-      <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName='login'>
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="login" component={Inicio} />
-        <Stack.Screen name="maps" component={maps} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
-
-
-
--- components/register.jsx
+++ components/register.jsx
@@ -1,181 +1,190 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
-    ScrollView, StyleSheet, TextInput, TouchableOpacity,
-    Text, View, Image, navigation, Dimensions, Platform, PixelRatio,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  Platform,
+  PixelRatio,
 } from "react-native";
-import { getCities, createUser } from "../funtion"
 
+/* Context */
+import { AuthContext } from "../navigation/AuthProvider";
 
 const Register = ({ navigation }) => {
-    getCities()
-    const [state, setState] = useState({ fName: "", fApellido: "", fCorreo: "", fPassword: "" });
-    const handleChange = (value, name) => {
-        setState(prevState => ({
-            ...prevState,
-            [name]: value
-        }));
-    };
-    const handleSubmit = () => {
-        createUser(state)
-        setState({ fName: "", fApellido: "", fCorreo: "", fPassword: "" })
-    }
-    return (
+  const [state, setState] = useState({
+    fName: "",
+    fApellido: "",
+    fCorreo: "",
+    fPassword: "",
+  });
 
-        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}>
-            <View style={styles.padreview}>
-                <Text style={[styles.textoRegister, styles.xlarge]}>Registrate</Text>
-                <TextInput
-                    style={styles.input}
-                    onChangeText={(value, name = 'fName') => handleChange(value, name)}
-                    placeholder="Nombre"
-                    value={state.fName}
-                    name={'fName'}
-                />
-                <TextInput
-                    style={styles.input}
-                    onChangeText={(value, name = 'fApellido') => handleChange(value, name)}
-                    placeholder="Apellido"
-                    value={state.fApellido}
-                    name={'fApellido'}
-                />
-                <TextInput
-                    style={styles.input}
-                    onChangeText={(value, name = 'fCorreo') => handleChange(value, name)}
-                    placeholder="Correo"
-                    value={state.fCorreo}
-                    name={'fCorreo'}
-                />
-                <TextInput
-                    style={styles.input}
-                    onChangeText={(value, name = 'fPassword') => handleChange(value, name)}
-                    value={state.fPassword}
-                    placeholder="contraseña"
-                    secureTextEntry={true}
-                    name={'fPassword'}
-                />
-                <View style={styles.view}>
-                    <TouchableOpacity
-                        style={styles.boton}
-                        onPress={() => navigation.navigate('login')}
-                    >
-                        <Text style={[styles.Text, styles.small]}>Iniciar Sesion</Text>
+  const { register } = useContext(AuthContext);
 
-                    </TouchableOpacity>
+  const handleChange = (value, name) => {
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const handleSubmit = () => {
+    register(state.fCorreo, state.fPassword);
+    setState({ fName: "", fApellido: "", fCorreo: "", fPassword: "" });
+  };
+  return (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+    >
+      <View style={styles.padreview}>
+        <Text style={[styles.textoRegister, styles.xlarge]}>Registrate</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={(value, name = "fName") => handleChange(value, name)}
+          placeholder="Nombre"
+          value={state.fName}
+          name={"fName"}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(value, name = "fApellido") =>
+            handleChange(value, name)
+          }
+          placeholder="Apellido"
+          value={state.fApellido}
+          name={"fApellido"}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(value, name = "fCorreo") => handleChange(value, name)}
+          placeholder="Correo"
+          value={state.fCorreo}
+          name={"fCorreo"}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={(value, name = "fPassword") =>
+            handleChange(value, name)
+          }
+          value={state.fPassword}
+          placeholder="contraseña"
+          secureTextEntry={true}
+          name={"fPassword"}
+        />
+        <View style={styles.view}>
+          <TouchableOpacity
+            style={styles.boton}
+            onPress={() => navigation.navigate("login")}
+          >
+            <Text style={[styles.Text, styles.small]}>Iniciar Sesion</Text>
+          </TouchableOpacity>
 
-                    <TouchableOpacity
-                        style={styles.boton2}
-                        onPress={handleSubmit}
-                    >
-                        <Text style={[styles.Text, styles.small]}>Siguiente</Text>
-
-                    </TouchableOpacity>
-                </View>
-                <View style={styles.viewimage} >
-                    <Image
-                        style={styles.imageEdit}
-                        source={require('../assets/personas.png')}
-                    >
-
-                    </Image>
-                </View>
-            </View>
-
-
-        </ScrollView>
-    );
+          <TouchableOpacity style={styles.boton2} onPress={handleSubmit}>
+            <Text style={[styles.Text, styles.small]}>Siguiente</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.viewimage}>
+          <Image
+            style={styles.imageEdit}
+            source={require("../assets/personas.png")}
+          ></Image>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
-const {
-    width: SCREEN_WIDTH,
-    height: SCREEN_HEIGHT,
-} = Dimensions.get('window');
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const scale = SCREEN_WIDTH / 320;
 
 export function normalize(size) {
-    const newSize = size * scale
-    if (Platform.OS === 'android') {
-        return Math.round(PixelRatio.roundToNearestPixel(newSize))
-    } else {
-        return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2
-    }
+  const newSize = size * scale;
+  if (Platform.OS === "android") {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize));
+  } else {
+    return Math.round(PixelRatio.roundToNearestPixel(newSize)) - 2;
+  }
 }
-const styles = StyleSheet.create({
-    mini: {
-        fontSize: normalize(12),
-    },
-    small: {
-        fontSize: normalize(15),
-    },
-    medium: {
-        fontSize: normalize(17),
-    },
-    large: {
-        fontSize: normalize(20),
-    },
-    xlarge: {
-        fontSize: normalize(24),
-    },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 0,
-        padding: 10,
-        paddingLeft: 20,
-        paddingRight: 20,
-        borderRadius: 50,
-        backgroundColor: '#FFFFFF',
 
-    },
-    padreview: {
-        paddingTop: 30,
-        paddingBottom: 30
-    },
-    boton: {
-        width: '45%',
-        height: 50,
-        margin: 10,
-        backgroundColor: "#1C4C96",
-        alignItems: "center",
-        borderRadius: 50,
-        justifyContent: "center",
-    },
-    boton2: {
-        width: '45%',
-        height: 50,
-        margin: 10,
-        backgroundColor: "#24C912",
-        alignItems: "center",
-        borderRadius: 50,
-        justifyContent: "center",
-    },
-    view: {
-        flexDirection: "row",
-        width: '100%',
-        justifyContent: 'center',
-    },
-    Text: {
-        /* fontSize: 18, */
-        color: "#ffff"
-    },
-    textoRegister: {
-        paddingLeft: 10,
-        /* fontSize: 40, */
-        color: '#ffff',
-        marginBottom: 10,
-        marginLeft: 17,
-        fontWeight: 'bold',
-    },
-    viewimage: {
-        /* position: 'absolute', */
-        alignSelf: 'center',
-        marginTop: 40,
-        /* bottom: '-0%' */
-    },
-    imageEdit: {
-        width: 286,
-        height: 189,
-    }
+const styles = StyleSheet.create({
+  mini: {
+    fontSize: normalize(12),
+  },
+  small: {
+    fontSize: normalize(15),
+  },
+  medium: {
+    fontSize: normalize(17),
+  },
+  large: {
+    fontSize: normalize(20),
+  },
+  xlarge: {
+    fontSize: normalize(24),
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 0,
+    padding: 10,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderRadius: 50,
+    backgroundColor: "#FFFFFF",
+  },
+  padreview: {
+    paddingTop: 30,
+    paddingBottom: 30,
+  },
+  boton: {
+    width: "45%",
+    height: 50,
+    margin: 10,
+    backgroundColor: "#1C4C96",
+    alignItems: "center",
+    borderRadius: 50,
+    justifyContent: "center",
+  },
+  boton2: {
+    width: "45%",
+    height: 50,
+    margin: 10,
+    backgroundColor: "#24C912",
+    alignItems: "center",
+    borderRadius: 50,
+    justifyContent: "center",
+  },
+  view: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "center",
+  },
+  Text: {
+    /* fontSize: 18, */
+    color: "#ffff",
+  },
+  textoRegister: {
+    paddingLeft: 10,
+    /* fontSize: 40, */
+    color: "#ffff",
+    marginBottom: 10,
+    marginLeft: 17,
+    fontWeight: "bold",
+  },
+  viewimage: {
+    /* position: 'absolute', */
+    alignSelf: "center",
+    marginTop: 40,
+    /* bottom: '-0%' */
+  },
+  imageEdit: {
+    width: 286,
+    height: 189,
+  },
 });
 
 export default Register;
